2c4ede50fc496727b6f940e1afcb038a86685578
Shreyas Vinaya <vinaya.shreyas@gmail.com>
2024-08-03 13:40:26 +0530

    fox

---
+++ runner.sh
@@ -0,0 +1,3 @@
+aizynthcli --smiles /Users/shreyasv/Desktop/research/deepchem/retrosynthesis/smiles.txt \
+--config /Users/shreyasv/Desktop/research/deepchem/retrosynthesis/aizynthfinder/models/config.yml \
+--nproc 4
