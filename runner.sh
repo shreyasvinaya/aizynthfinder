495961bb97851faa4a7d58e7e60b87a33140f0fd
Shreyas Vinaya <vinaya.shreyas@gmail.com>
2025-01-23 19:52:02 +0530

    cleanup

--- runner.sh
+++
@@ -1,3 +0,0 @@
-aizynthcli --smiles /Users/shreyasv/Desktop/research/deepchem/retrosynthesis/smiles.txt \
---config /Users/shreyasv/Desktop/research/deepchem/retrosynthesis/aizynthfinder/models/config.yml \
---nproc 4
